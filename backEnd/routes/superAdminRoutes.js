4ce7bca725783176312712d666ee745b186a392b
Jeon Drew Jaime <89388113+JeonDrewJaime@users.noreply.github.com>
2024-11-13 04:01:31 +0800

    DELETING MVC FOR BASIC SHIT

--- backEnd/routes/superAdminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const SuperAdminController = require('../controllers/SuperAdminController');
-
-// Define the POST route to create a user
-router.post('/create-user', SuperAdminController.createUser);
-
-module.exports = router;
